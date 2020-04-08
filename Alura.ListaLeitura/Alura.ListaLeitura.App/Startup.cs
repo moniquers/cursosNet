77bba995aa1590df7fbd504359894a26f1bcd32a
Monique Ribeiro Sousa <ribeiromonique90@gmail.com>
2020-04-08 18:27:08 +0000

    roteamento: atualiza roteamento com Dictionary para o  o serviço de roteamento do AspNet Core

--- Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
+++ Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
@@ -1,38 +1,74 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Alura.ListaLeitura.App.Negocio;
 using Alura.ListaLeitura.App.Repositorio;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Alura.ListaLeitura.App
 {
     public class Startup
     {
+
+
+        public void ConfigureServices(IServiceCollection service)
+        {
+            service.AddRouting();
+        }
+
         public void Configure(IApplicationBuilder app)
         {
-            app.Run(Roteamento);
+            var routeBuilder = new RouteBuilder(app);
+
+            routeBuilder.MapRoute("Livros/ParaLer", LivrosParaLer);
+            routeBuilder.MapRoute("Livros/Lidos", LivrosLidos);
+            routeBuilder.MapRoute("Livros/Lendo", LivrosLendo);
+            routeBuilder.MapRoute("Cadastro/NovoLivro/{nome}/{autor}", NovoLivroParaLer);
+
+            var rotas = routeBuilder.Build();
+
+            app.UseRouter(rotas);
+
+            //app.Run(Roteamento);
 
         }
 
-        public Task Roteamento(HttpContext context)
+        public Task NovoLivroParaLer(HttpContext context)
         {
-            var _repo = new LivroRepositorioCSV();
-            var caminhosAtendidos = new Dictionary<string, RequestDelegate>
+            var livro = new Livro()
             {
-                {"/Livros/ParaLer", LivrosParaLer},
-                {"/Livros/Lendo", LivrosLendo},
-                {"/Livros/Lidos", LivrosLidos}
+                Titulo = Convert.ToString(context.GetRouteValue("nome")),
+                Autor = Convert.ToString(context.GetRouteValue("autor"))
+                
             };
 
-            if (caminhosAtendidos.ContainsKey(context.Request.Path))
-            {
-                var metodo = caminhosAtendidos[context.Request.Path];
-                return metodo.Invoke(context);
-            }
-            context.Response.StatusCode = 404;
-            return context.Response.WriteAsync("Caminho inexistente.");
-
+            var repo = new LivroRepositorioCSV();
+            repo.Incluir(livro);
+            return context.Response.WriteAsync("O livro foi adicionado com sucesso");
         }
+
+        //public Task Roteamento(HttpContext context)
+        //{
+        //    var _repo = new LivroRepositorioCSV();
+        //    var caminhosAtendidos = new Dictionary<string, RequestDelegate>
+        //    {
+        //        {"/Livros/ParaLer", LivrosParaLer},
+        //        {"/Livros/Lendo", LivrosLendo},
+        //        {"/Livros/Lidos", LivrosLidos}
+        //    };
+
+        //    if (caminhosAtendidos.ContainsKey(context.Request.Path))
+        //    {
+        //        var metodo = caminhosAtendidos[context.Request.Path];
+        //        return metodo.Invoke(context);
+        //    }
+        //    context.Response.StatusCode = 404;
+        //    return context.Response.WriteAsync("Caminho inexistente.");
+
+        //}
 
         public Task LivrosParaLer(HttpContext context)
         {
